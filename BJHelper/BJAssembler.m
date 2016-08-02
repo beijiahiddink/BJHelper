8c3e917a382373bd42b237de5f1950d1317b274c
王旭 <beijiahiddink@163.com>
2016-08-02 13:25:44 +0800

    安装包减负

--- BJHelper/BJAssembler.m
+++ BJHelper/BJAssembler.m
@@ -1,9 +1,12 @@
 //
 //  BJAssembler.m
-//  BJHelper
+//  BJHelper <https://github.com/beijiahiddink/BJHelper>
 //
 //  Created by 王旭 on 16/7/30.
 //  Copyright © 2016年 beijiahiddink. All rights reserved.
+//
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
 //
 
 #import "BJAssembler.h"
--- BJHelper/main.m
+++ BJHelper/main.m
@@ -1,9 +1,12 @@
 //
 //  main.m
-//  BJHelper
+//  BJHelper <https://github.com/beijiahiddink/BJHelper>
 //
 //  Created by 王旭 on 16/7/27.
 //  Copyright © 2016年 beijiahiddink. All rights reserved.
+//
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
 //
 
 #import <UIKit/UIKit.h>
